f7c36118a3dff4f5f3573d32a3fb9585b15f1120
francesco <francesco.pinsone@alcampus.it>
2022-11-30 12:41:43 +0100

    convert_roman_to_arabic() logic & __main__

--- main.py
+++ main.py
@@ -5,5 +5,52 @@ def convert_roman_to_arabic(roman: str) -> str:
     :param roman: string to be converted
     :return: converted string
     """
-    # inserisci qui il tuo codice
-    pass
+
+    # Creo un dizionario per tener traccia degli elementi base del sistema a numeri romani
+    # e dei corrispondenti valori. Cosi da poter effettuare le operazioni in modo più semplice
+    roman_number = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+    arabic = 0  # numero convertito inizializzato a 0
+
+    if check_roman_number(roman):  # se il check è andato a buon fine eseguo la conversione
+        i = 0  # Contatore per indice stringa in input
+
+        while i < len(roman):
+            # controllo che l'elemento in considerazione sia corrispondente a una delle chiavi
+            # di roman_number:
+            if roman[i] in roman_number.keys():
+                roman_current_value = roman_number.get(roman[i])
+
+                # Controllo se i corrisponde all'ultimo elemento della stringa da convertire
+                # in tal caso forzo l'elemento successivo a 0 in modo da non avere un errore di
+                # IndexError
+                if i == len(roman) - 1:  # se sono arrivato alla fine dell'array roman
+                    roman_next_value = 0
+                else:
+                    roman_next_value = roman_number.get(roman[i + 1])
+
+                # Se il valore corrente è minore di quello successivo nella stringa allora devo
+                # sommare la differenza tra il valore successivo e quello corrente
+                # Altrimenti posso semplicemente sommare il valore corrente
+                if roman_current_value < roman_next_value:
+                    arabic += roman_next_value - roman_current_value
+                    i += 2
+                else:
+                    arabic += roman_current_value
+                    i += 1
+    return arabic
+
+
+def check_roman_number(roman: str) -> bool:
+    return True
+
+
+if __name__ == '__main__':
+    exit_cond = 0  # condizione di terminazione del programma
+    while exit_cond == 0:
+        x = input("Digita un numero romano da convertire: ")
+        if x != 'fine':
+            print(convert_roman_to_arabic(x))
+        else:
+            exit_cond = 1
+            print("processo terminato..")
